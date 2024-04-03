d3802d452de95d6a478cb3b331a6e1ae415606b7
Matt Hibbs <mhibbs@trinity.edu>
2024-04-03 14:21:33 -0500

    Finished map stuff

--- app/src/main/java/cs2/util/MapStuff.java
+++ app/src/main/java/cs2/util/MapStuff.java
@@ -9,8 +9,22 @@ public class MapStuff {
     map.put('A',"apple");
     map.put('C',"carrot");
     map.put('B',"banana");
+    map.put('D',"apple");
     System.out.println(map);
-    
+    System.out.println(map.containsKey('A'));
+    System.out.println(map.containsKey('Q'));
+    System.out.println(map.containsValue("apple"));
+    System.out.println(map.containsValue("Apple"));
+    System.out.println(map.size());
+    System.out.println(map.get('B'));
+    System.out.println(map.get('Q'));
+    map.put('A',"aardvark");
+    System.out.println(map);
+    System.out.println(map.keySet());
 
+    for(Character key : map.keySet()) {
+      System.out.println(key + " has value " + map.get(key));
+    }
+  
   }
 }
--- app/src/main/java/cs2/util/TextAnalysis.java
+++ app/src/main/java/cs2/util/TextAnalysis.java
@@ -25,6 +25,35 @@ public class TextAnalysis {
     return words;
   }
 
+  public static HashMap<String, Integer> getWordCounts(String filename) {
+    HashMap<String,Integer> counts = new HashMap<String,Integer>();
+    try {
+      Scanner fin = new Scanner(new File(filename));
+      while(fin.hasNextLine()) {
+        String line = fin.nextLine();
+        System.out.println(line);
+        String[] parts = line.split("\\s");
+        for(int i=0; i<parts.length; i++) {
+          String word = parts[i].replaceAll("[^A-Za-z]","").toLowerCase();
+
+          if(!word.equals("")) {
+            if(counts.containsKey(word)) {
+              //counts[word] += 1
+              //counts[word] = counts[word] + 1
+              counts.put(word, counts.get(word) + 1);
+            } else {
+              //counts[word] = 1
+              counts.put(word, 1);
+            }
+          }
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return counts;
+  }
+
   public static void main(String[] args) {
     HashSet<String> temp = getWords("tempest.txt");
     HashSet<String> mid = getWords("midsummer.txt");
@@ -46,6 +75,23 @@ public class TextAnalysis {
       System.out.println(word);
     }
     System.out.println(temp.size());
+
+    HashMap<String,Integer> tempCounts = getWordCounts("tempest.txt");
+    System.out.println(tempCounts);
+
+    String freqWord = "ZZZZZZ";
+    for(String key : tempCounts.keySet()) {
+      if(tempCounts.get(freqWord) == null || tempCounts.get(key) > tempCounts.get(freqWord)) {
+        freqWord = key;
+      }
+    }
+    System.out.println(freqWord + " occurs " + tempCounts.get(freqWord) + " times");
+
+    for(Map.Entry<String,Integer> entry : tempCounts.entrySet()) {
+      if(entry.getValue() > 100) {
+        System.out.println(entry.getKey() + "\t\t" + entry.getValue());
+      }
+    }
 
   }
 }
